23ec4fc574ec3fa187c12698b81b620346f170da
cruizsan <carlos.ruizsanchez@unamur.be>
2020-01-28 16:57:38 +0100

    rebuild image no cache

--- github-runner/buil-docker-image.sh
+++ github-runner/buil-docker-image.sh
@@ -4,4 +4,4 @@ dir=`dirname "$(realpath $0)"`
 name=github-runner
 version=v0.0.1
 
-docker image build -t ${name}:${version} ${dir}
+docker image build --no-cache -t ${name}:${version} ${dir}
--- github-runner/entrypoint.sh
+++ github-runner/entrypoint.sh
@@ -4,6 +4,8 @@ service docker start
 
 export AGENT_ALLOW_RUNASROOT=1
 
+echo "$AGENT_ALLOW_RUNASROOT"
+
 if [ ! -f .configured ]; then
   echo ${RUNNER_NAME:-default}$'\n\n' | /config.sh --url ${REPO_URL} --token ${RUNNER_TOKEN}
   touch .configured
